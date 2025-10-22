2930178cd199fe62344afc8d0e7ac4c0c595a65b
mostaque123 <mostaqueahmad121@gmail.com>
2025-10-22 10:35:49 +0600

    runtime issue solve in proxy

--- proxy.ts
+++ proxy.ts
@@ -32,7 +32,6 @@ export async function proxy(request: NextRequest) {
 }
 
 export const config = {
-  runtime: "nodejs",
   matcher: [
     /*
      * Match all request paths except for the ones starting with:
